082aee431aa44cc2aad0617ad187805871e7718f
Patrick Bennett <pb@patricks-mbp.attlocal.net>
2019-09-27 16:19:30 -0400

    Tons of fucking initial work Too much to write about here Will commit more often

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 var proxy = require('http-proxy-middleware')
+const path = require('path')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Gabler Youngston Website',
     description:
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/gy_logo.png'
 
-const Navbar = class extends React.Component {
+const Nav = class extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -42,7 +41,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="Gabler Youngston" />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -59,34 +58,19 @@ const Navbar = class extends React.Component {
             id="navMenu"
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
             <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
+                <Link activeClassName="is-active" className="navbar-item" to="/about">
+                  ABOUT
+                </Link>
+                <Link activeClassName="is-active" className="navbar-item" to="/work">
+                  WORK
+                </Link>
+                <Link activeClassName="is-active" className="navbar-item" to="/news">
+                  NEWS
+                </Link>
+                <Link activeClassName="is-active" className="navbar-item" to="/contact">
+                  CONTACT
+                </Link>
             </div>
           </div>
         </div>
@@ -95,4 +79,4 @@ const Navbar = class extends React.Component {
   }
 }
 
-export default Navbar
+export default Nav
