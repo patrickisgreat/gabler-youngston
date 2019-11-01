0b3e1cc98f59bf9b8d73d25fcd9838998f8c7563
Atul sharma <atulshrma91@gmail.com>
2019-11-01 14:15:17 +0530

    About page layout

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -5,7 +5,10 @@ import Layout from '../components/Layout'
 import { HTMLContent } from '../components/Content'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage';
 
-export const AboutPageTemplate = ({ title, image, contentComponent, section1, section2 }) => {
+export const AboutPageTemplate = ({ title, image, contentComponent, section1, section2, section3, section4, section5 }) => {
+  console.log(section3)
+  console.log(section4)
+  console.log(section5)
   return (
     <div className="about-slides">
       <div className="content">
@@ -104,6 +107,60 @@ export const AboutPageTemplate = ({ title, image, contentComponent, section1, se
         </div>
         </div>
       </section>
+      <section className="section-three">
+      <h4 className="title right">{section3.title}</h4>
+      <div className="columns">
+          {section3.images.map((image, i) => {
+            return (
+              <div className="column is-3" key={i}>
+                <PreviewCompatibleImage
+                  className="gallery-img"
+                  imageInfo={{
+                    image: !!image.image.childImageSharp
+                      ? image.image.childImageSharp.fluid.src
+                      : image.image,
+                    alt: `Gallery Test`,
+                    style: {
+                      borderRadius: "0px",
+                      maxWidth: "100%"
+                    }
+                  }}
+                />
+
+            </div>
+            );
+          })}
+
+         </div>
+      </section>
+      <section className="section-four">
+      <h4 className="title">{section4.title}</h4>
+      <div className="description">
+          <ul>
+          {section4.description.map((description, i) => {
+            return (
+              <li  key={i}>
+              {description.text}
+              </li>
+            );
+          })}
+          </ul>
+      </div>
+      </section>
+      <section className="section-five">
+      <h4 className="title">{section5.title}</h4>
+      <div className="description">
+        <ul>
+        {section5.description.map((description, i) => {
+          return (
+            <li  key={i}>
+            {description.text}
+            </li>
+          );
+        })}
+        </ul>
+      </div>
+      </section>
     </div>
   )
 }
@@ -126,6 +183,18 @@ AboutPageTemplate.propTypes = {
     image3: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
     image4: PropTypes.oneOfType([PropTypes.object, PropTypes.string])
   }),
+  section3: PropTypes.shape({
+    title: PropTypes.string,
+    images: PropTypes.array
+  }),
+  section4: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.array
+  }),
+  section5: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.array
+  }),
 }
 
 const AboutPage = ({ data }) => {
@@ -138,6 +207,9 @@ const AboutPage = ({ data }) => {
         image={frontmatter.image}
         section1={frontmatter.section1}
         section2={frontmatter.section2}
+        section3={frontmatter.section3}
+        section4={frontmatter.section4}
+        section5={frontmatter.section5}
       />
     </Layout>
   )
@@ -206,6 +278,30 @@ export const aboutPageQuery = graphql`
                 ...GatsbyImageSharpFluid
               }
             }
+          }
+        }
+        section3 {
+          title
+          images{
+            image {
+              childImageSharp {
+                fluid(maxWidth: 2048, quality: 100) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+        section4 {
+          title
+          description{
+            text
+          }
+        }
+        section5 {
+          title
+          description{
+            text
           }
         }
       }
