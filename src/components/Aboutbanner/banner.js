74d14ba0c34c1b59c638ca84d6b91c57041a5e88
Ashishmehan <ashish.tws@tekkiwebsolutions.com>
2019-11-15 17:26:34 +0530

    Add about us page banner image

--- src/components/Aboutbanner/banner.js
+++ src/components/Aboutbanner/banner.js
@@ -5,7 +5,7 @@ const AboutBanner = class extends React.Component {
   render() {
     return (
       <div>
-        <div className="Banner-min" style={{ background: 'url(.././img/about-banner.jpg) no-repeat center center'}}>
+        <div className="Banner-min" >
           <div className="container">
               <h1>Our <span>Studio</span></h1>
           </div>
