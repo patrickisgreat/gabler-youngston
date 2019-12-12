822e8b89b6a45abd9968274b5b99279b0c6cb929
Ashishmehan <54096860+Ashishmehan@users.noreply.github.com>
2019-12-12 18:24:59 +0530

    Update Newsletter.js

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -71,7 +71,7 @@ class Newsletter extends React.Component {
                      <h5>NEWSLETTER</h5>
                 </div>
                 <div className="column is-5">
-                <form name="subscribe" action="/about" method="post" data-netlify="true" data-netlify-honeypot="bot-field" onChange={this.contactSubmit.bind(this)}>
+                <form name="subscribe" action="/about" method="post" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={this.contactSubmit.bind(this)}>
                   <input type="hidden" name="bot-field" />
                   <input type="hidden" name="form-name" value="subscribe" />
                   <div className="form-group">
