20cabb0bcee7bba51a38713d473e6d66bb5df4c9
Patrick Bennett <patrick.bennett@soundcloud.com>
2024-04-02 12:30:29 -0400

    fix: previews

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -3,21 +3,30 @@ import PropTypes from "prop-types";
 import { IndexPageTemplate } from "../../templates/index-page";
 
 const IndexPagePreview = ({ entry, getAsset, widgetsFor }) => {
-    const image = entry.getIn(["data", "image"]);
-    const title = entry.getIn(["data", "title"]);
-    const slides = entry.getIn(["data", "slides"]).toJS();
-    const galleryImages = entry.getIn(["data", "galleryImages"]).toJS();
-    const footerWorks = entry.getIn(["data", "footerWorks"]).toJS();
+  const image = entry.getIn(["data", "image"]);
+  const title = entry.getIn(["data", "title"]);
+  const slides = entry.getIn(["data", "slides"]).toJS();
+  const galleryImages = entry.getIn(["data", "galleryImages"]).toJS();
+  const footerWorks = entry.getIn(["data", "footerWorks"]).toJS();
 
-    return (
-      <IndexPageTemplate
-        image={image}
-        title={title}
-        slides={slides}
-        galleryImages={galleryImages}
-        footerWorks={footerWorks}
-      />
-    );
+  console.log("entry:", entry);
+  console.log("getAsset:", getAsset);
+  console.log("widgetsFor:", widgetsFor);
+  console.log("image:", image);
+  console.log("title:", title);
+  console.log("slides:", slides);
+  console.log("galleryImages:", galleryImages);
+  console.log("footerWorks:", footerWorks);
+
+  return (
+    <IndexPageTemplate
+      image={image}
+      title={title}
+      slides={slides}
+      galleryImages={galleryImages}
+      footerWorks={footerWorks}
+    />
+  );
 };
 
 IndexPagePreview.propTypes = {
